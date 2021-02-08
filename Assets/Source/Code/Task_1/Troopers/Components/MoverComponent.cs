0a692c9c68514fd5a0673fa98f1f9d05a340af86
artUSUN <a.d.levadniy@gmail.com>
2021-02-08 22:23:26 +0300

    MoverComponent

---
+++ Assets/Source/Code/Task_1/Troopers/Components/MoverComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Source.Code.Task_1.Troopers.Components
+{
+    public class MoverComponent
+    {
+        private readonly Transform tr;
+        private readonly Rigidbody rb;
+        private readonly float speed;
+
+        public MoverComponent(TrooperBehaviour trooper)
+        {
+            tr = trooper.Transform;
+            rb = trooper.GetComponent<Rigidbody>();
+            speed = trooper.Settings.MoveSpeed;
+        }
+
+        public void Move(Vector3 targetPoint)
+        {
+            SetRotationToTarget(targetPoint);
+
+            rb.AddForce(tr.forward * speed * Time.fixedDeltaTime);
+
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed); 
+        }
+
+        private void SetRotationToTarget(Vector3 targetPoint)
+        {
+            targetPoint.y = tr.position.y;
+            tr.LookAt(targetPoint);
+        }
+    }
+}
--- Assets/Source/Code/Task_1/Troopers/TrooperBehaviour.cs
+++ Assets/Source/Code/Task_1/Troopers/TrooperBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using Source.Code.Task_1.Systems;
 using Source.Code.Task_1.Troopers.Components;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,14 @@ namespace Source.Code.Task_1.Troopers
 {
     public class TrooperBehaviour : MonoBehaviour
     {
+        [SerializeField] private TrooperSettings settings;
+
         private Faction faction;
         private TargetSelectionComponent targetSelectionComponent;
 
         public TrooperBehaviour Target { get; private set; }
         public Transform Transform { get; private set; }
+        public TrooperSettings Settings => settings;
         
 
         public void Initialize(Faction faction, TargetSelectionComponent targetSelectionComponent)
@@ -30,4 +34,19 @@ namespace Source.Code.Task_1.Troopers
             }
         }
     }
+
+    [Serializable]
+    public struct TrooperSettings
+    {
+        [Min(1)] [SerializeField] private float maxHP;
+        [Min(0)] [SerializeField] private float damagePerSecond;
+        [Min(0)] [SerializeField] private float attackRange;
+        [Min(0)] [SerializeField] private float speed;
+
+        public float MaxHP => maxHP;
+        public float DamagePerSecond => damagePerSecond;
+        public float AttackRange => attackRange;
+        public float MoveSpeed => speed;
+    }
+
 }
